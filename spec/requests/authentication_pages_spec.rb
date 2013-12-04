65637c980f7d54bb1ba5289823986252debe6726
Dan Cleary <dcc635@gmail.com>
2013-12-04 15:35:28 -0800

    Completed Exercise 9.3

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,6 +7,8 @@ describe "Authentication" do
   describe "signin" do
     before { visit signin_path }
 
+
+
     describe "with invalid information" do
       before { click_button "Sign in" }
       it { should have_title('Sign in') }
@@ -39,6 +41,13 @@ describe "Authentication" do
 
       describe "for non-signed-in users" do
         let(:user) { FactoryGirl.create(:user) }
+
+        it { should_not have_title(user.name) }
+        it { should_not have_link('Users',        href: users_path) }
+        it { should_not have_link('Profile',      href: user_path(user)) }
+        it { should_not have_link('Settings',     href: edit_user_path(user)) }
+        it { should_not have_link('Sign out',     href: signout_path) }
+        it { should have_link('Sign in',          href: signin_path) }
 
         describe "when attempting to visit a protected page" do
           before do
